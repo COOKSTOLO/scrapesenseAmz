81b1f65fc1d53ca0bbb4933d025e0fa0f3371b74
Cookstolo <106170598+COOKSTOLO@users.noreply.github.com>
2025-03-26 17:19:27 -0600

    hoy

--- bot.py
+++ bot.py
@@ -333,7 +333,7 @@ for url in urls[:]:  # Usamos una copia de la lista para evitar problemas al mod
             urls.remove(url)  # También removemos la URL si no se encuentra el precio
         
         # Esperar 15 minutos (450 segundos) antes de la siguiente iteración
-        time.sleep(900)
+        time.sleep(1300)
         
         # Actualizar el archivo JSON (guardamos la lista actualizada)
         with open('enlacesAfiliadoAmz.json', 'w') as file:
